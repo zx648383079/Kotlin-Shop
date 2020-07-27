e596feeb6bc764718e744f8e6b0ad0e486ddf238
ZoDream <648383079@qq.com>
2020-07-27 18:06:17 +0800

    update

---
+++ src/app/src/main/java/com/zodream/shop/pages/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.zodream.shop.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zodream.shop.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
